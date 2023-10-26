e469d38f155e3ed7f600b2aa25470a9bfc748e48
Jonny Saykosy <jsaykosy@sofya.fr>
2023-10-26 17:54:44 +0200

    75:46: Adding the Article Component

---
+++ angular-hello-world/src/app/user-item/user-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css'],
+})
+export class UserItemComponent {
+  @Input() name: string = '';
+
+  constructor() {}
+}
